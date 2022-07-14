0fcb02ad69a549bf1624d7d9d581e0b25b048ec7
Baldemyr <son_of_loki@hotmail.com>
2022-07-13 22:26:00 -0400

    Fixing Thought.js under models

--- models/thought.js
+++ models/thought.js
@@ -1,3 +1,11 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./reaction');
 const moment = require('moment');
+
+const thoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 250
+    },
