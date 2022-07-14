ac7779ae171cd5122b1dafd54041dab472b38b7b
Baldemyr <son_of_loki@hotmail.com>
2022-07-13 22:37:47 -0400

    Managed to run it in terminal but still not sure g=of what constitutes it working.  I found a lot of simple syntax errors on my part

--- models/reaction.js
+++ models/reaction.js
@@ -6,3 +6,28 @@ const reactionSchema = new Schema ({
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId()
     },
+reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+},
+username: {
+    type: String,
+    required: true
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+}
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+}
+
+);
+
+module.exports = reactionSchema;
