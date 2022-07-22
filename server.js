dc0a6aa8fc464eb88049f184e44eb2a756159be7
Baldemyr <son_of_loki@hotmail.com>
2022-07-22 13:04:24 -0400

    Further work

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+// require and initalize necessary connections
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
+app.use(express.json());
+app.use(routes);
+
+//display msg using console log once app is "listeing"
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
